e399b4a58fa6901ea59a6c64e649b594462a27a5
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2021-07-12 03:49:18 -0300

    update

--- start/kernel.ts
+++ start/kernel.ts
@@ -43,4 +43,5 @@ Server.middleware.register([
 */
 Server.middleware.registerNamed({
     auth: () => import('App/Middleware/Auth'),
+    adminControl: () => import('App/Middleware/AdminControl'),
 });
